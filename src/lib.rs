ae3c05d68c8bcba30c66c9eaa2367b5f05d6ed85
DoceAzedo <me@doceazedo.com>
2024-06-02 11:54:09 -0300

    Add POST /subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-use actix_web::dev::Server;
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
+
+use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, Result, web};
+use actix_web::dev::Server;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct SubscribeRequest {
+    name: String,
+    email: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -17,15 +25,21 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[post("/subscribe")]
+async fn subscribe(req: web::Json<SubscribeRequest>) -> Result<String> {
+    Ok(format!("Hey {} ({})!", req.name, req.email))
+}
+
 pub fn get_server(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .service(health_check)
+            .service(subscribe)
     })
-    .listen(listener)?
-    .run();
+        .listen(listener)?
+        .run();
     Ok(server)
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
-use actix_web::rt::spawn;
+use std::collections::HashMap;
 use std::net::TcpListener;
+
+use actix_web::rt::spawn;
+
 use zero2prod::get_server;
 
 fn spawn_app() -> String {
@@ -15,7 +18,51 @@ async fn health_check_works() {
     let url = spawn_app();
     let status = reqwest::get(format!("{}/health_check", &url))
         .await
-        .unwrap()
+        .expect("Could not make request")
         .status();
     assert!(status.is_success());
+}
+
+#[actix_web::test]
+async fn subscribe_returns_200_for_valid_data() {
+    let url = spawn_app();
+
+    let mut body = HashMap::new();
+    body.insert("email", "doce@example.com");
+    body.insert("name", "Doce");
+
+    let client = reqwest::Client::new();
+    let status = client.post(format!("{}/subscribe", &url))
+        .json(&body)
+        .send()
+        .await
+        .expect("Could not make request")
+        .status();
+    assert!(status.is_success());
+}
+
+#[actix_web::test]
+async fn subscribe_returns_400_for_invalid_data() {
+    let url = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("body with no email", "name", "Doce"),
+        ("body with no name", "email", "doce@example.com"),
+        ("empty body", "", ""),
+    ];
+
+    for (case, key, value) in test_cases {
+        let mut body = HashMap::new();
+        if !key.is_empty() && !value.is_empty() {
+            body.insert(key, value);
+        }
+        let status = client.post(format!("{}/subscribe", &url))
+            .json(&body)
+            .send()
+            .await
+            .expect("Could not make request")
+            .status();
+        assert_eq!(status.as_u16(), 400, "API did not return 400 when request has {}", case);
+    }
 }
